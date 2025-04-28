57cb0269356c09ab3ebc5e1132a41d080ca27516
Gilbert Virgo <gilbertjvirgo@gmail.com>
2025-04-28 21:53:38 +0100

    Replaced Soliana image (another)

--- src/pages/WhatsOn/blocks.js
+++ src/pages/WhatsOn/blocks.js
@@ -118,7 +118,7 @@ export default (showModal) => [
 		],
 		images: [
 			{
-				src: "/assets/images/whatson_filtered.jpeg",
+				src: "/assets/images/beliefs_filtered.JPG",
 				alt: "A VPCC Sunday Service",
 			},
 		],
