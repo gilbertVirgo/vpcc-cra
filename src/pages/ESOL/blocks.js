5c83a1e5d7f6a3b240efafa46d808f7c8b85f1d3
Gil <gilbertjvirgo@gmail.com>
2026-04-13 14:31:44 +0100

    Updated ESOL page

--- src/pages/ESOL/blocks.js
+++ src/pages/ESOL/blocks.js
@@ -5,8 +5,8 @@ export default [
 		type: "custom",
 		body: (
 			<div className="wrapper">
-				<div className="max-w-2xl flex flex-col gap-6">
-					<div className="flex flex-col gap-4 px-8">
+				<div className="max-w-2xl flex flex-col gap-8">
+					<div className="flex flex-col gap-3 px-8">
 						<h1>ESOL</h1>
 						<h3>Join us for a friendly, relaxed English course.</h3>
 					</div>
@@ -16,28 +16,38 @@ export default [
 					/>
 
 					<p className="px-8">
-						This is a brand new course run by Rachel, who has lived
-						in East London for nearly three decades and has led a
-						number of similar courses in the in recent years.
+						This is a brand new course run by Rachel, a qualified
+						teacher who has lived in East London for nearly three
+						decades and has led a number of similar courses in the
+						in recent years.
 					</p>
 
-					<ul className="flex flex-col gap-1 pr-8 pl-14 list-disc">
-						<li>
-							<strong>When:</strong> Every Friday, 11am–2pm
-						</li>
-						<li>
-							<strong>Taster session:</strong> Friday 17th April
-						</li>
-						<li>
-							<strong>Length:</strong> 8 weeks
-						</li>
-						<li>
-							<strong>Cost:</strong> Free
-						</li>
-					</ul>
+					<div className="flex flex-col gap-3 px-8">
+						<h3>Details</h3>
+						<ul className="flex flex-col gap-1 pl-6 list-disc">
+							<li>
+								<strong>When:</strong> Fridays from 11am to 2pm
+							</li>
+							<li className="mb-2">
+								<strong>Where:</strong> 17 Lark Row, London, E2
+								9JA
+							</li>
+							<li>
+								<strong>Course start date:</strong> Friday 17th
+								April
+							</li>
 
-					<div className="flex flex-col gap-4 px-8">
-						<h3>What to expect:</h3>
+							<li>
+								<strong>Length:</strong> 8 weeks
+							</li>
+							<li>
+								<strong>Cost:</strong> Free
+							</li>
+						</ul>
+					</div>
+
+					<div className="flex flex-col gap-3 px-8">
+						<h3>What to expect</h3>
 						<ul className="flex flex-col gap-1 pl-6 list-disc">
 							<li>Conversation practice</li>
 							<li>Cooking together</li>
@@ -46,19 +56,37 @@ export default [
 						</ul>
 					</div>
 
-					<div className="flex flex-col gap-4 px-8">
-						<p>
-							<strong>Places are limited</strong>, so please sign
-							up soon.
-						</p>
+					<div className="flex flex-col gap-3 px-8">
+						<h3>Sign up</h3>
+						<div className="flex flex-col gap-2 ">
+							<p>
+								<strong>Places are limited</strong>, so please
+								sign up soon.
+							</p>
 
-						<div className="flex flex-col md:flex-row gap-4">
-							<Button href="tel:+447906875505">
-								Call Rachel
-							</Button>
-							<Button href="/connect">Contact us</Button>
+							<p>
+								If you would like to join us, text 'ESOL' with
+								your name to{" "}
+								<a href="sms:+447906875505">07906 875505</a>.
+							</p>
 						</div>
+						<div className="flex flex-col gap-3 ">
+							<p>
+								Alternatively, you can contact us via phone or
+								email:
+							</p>
 
+							<div className="flex flex-col md:flex-row gap-3">
+								<Button href="tel:+447906875505">
+									Call Rachel
+								</Button>
+								<Button href="/connect">
+									Contact us via email
+								</Button>
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col gap-3 px-8">
 						<p>Everyone is welcome — we’d love to meet you!</p>
 					</div>
 				</div>
